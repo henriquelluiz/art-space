90737758ef03ddc45377d9ed6f00ca309ff7102b
Henrique Luiz <heenluy@gmail.com>
2023-12-12 17:33:20 -0300

    set a basic layout for the app

--- app/src/main/java/me/henriquelluiz/artspace/MainActivity.kt
+++ app/src/main/java/me/henriquelluiz/artspace/MainActivity.kt
@@ -3,13 +3,26 @@ package me.henriquelluiz.artspace
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ElevatedCard
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import me.henriquelluiz.artspace.ui.theme.ArtSpaceTheme
 
 class MainActivity : ComponentActivity() {
@@ -21,7 +34,11 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    ArtSpaceLayout(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .wrapContentSize(Alignment.Center)
+                    )
                 }
             }
         }
@@ -29,17 +46,59 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun ArtSpaceLayout(modifier: Modifier = Modifier) {
+    Column(
         modifier = modifier
-    )
+    ) {
+
+        ElevatedCard(
+            elevation = CardDefaults.cardElevation(
+                defaultElevation = 0.dp
+            ),
+            modifier = Modifier
+                .padding(0.dp)
+
+        ) {
+            Image(
+                painter = painterResource(R.drawable.picture_1),
+                contentDescription = null
+            )
+        }
+
+        Card {
+            Text(
+                text = stringResource(R.string.title_pic_1)
+            )
+
+            Row {
+                Text(text = stringResource(R.string.artist_pic_1))
+                Text(text = stringResource(R.string.year_pic_1))
+            }
+        }
+
+        Row {
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Previous")
+            }
+
+            Button(onClick = { /*TODO*/ }) {
+                Text(text = "Next")
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    showSystemUi = true
+)
 @Composable
-fun GreetingPreview() {
+fun ArtSpaceLayoutPreview() {
     ArtSpaceTheme {
-        Greeting("Android")
+        ArtSpaceLayout(
+            modifier = Modifier
+                .fillMaxSize()
+                .wrapContentSize(Alignment.Center)
+        )
     }
 }
